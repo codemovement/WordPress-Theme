809ba4d7ea653039e9d2179105b6a7989e71293c
Masood U <Masood.Usama@datum.pk>
2016-05-24 23:40:10 +0500

    cleanup js and css file

--- js/main.js
+++ js/main.js
@@ -5,13 +5,7 @@ jQuery(document).ready(function($) {
 	
 	$('.grid').masonry({
 	  // set itemSelector so .grid-sizer is not used in layout
-	  itemSelector: '.grid-item',
-	  // use element for option
-	  //columnWidth: '.grid-sizer',
-	  // percentPosition: true,
-	  // gutter: 10,
-	  // fitWidth: true,
-	  // columnWidth: 120
+	  itemSelector: '.grid-item'
 	});
 
 });
